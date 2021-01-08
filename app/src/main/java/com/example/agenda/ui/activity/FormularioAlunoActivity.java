bdb28f5ca85fc6f860293bbd2bae980ff42aa412
RafaelKSalvador <rafaelk.salvador@gmail.com>
2021-01-08 12:05:43 -0300

    Android parte 1: Crie a sua primeira app mobile

--- app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,13 +2,12 @@ package com.example.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
 
 import com.example.agenda.R;
 import com.example.agenda.dao.AlunoDAO;
@@ -16,32 +15,50 @@ import com.example.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
+
+    public static final String TITULO_APPBAR = "Novo aluno";
+    private EditText campoNome;
+    private EditText campoTelefone;
+    private EditText campoEmail;
+    private final AlunoDAO dao = new AlunoDAO();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+        setTitle(TITULO_APPBAR);
+        inicializacaoDosCampos();
+        configuraBotaoSalvar();
+    }
 
-        final AlunoDAO dao = new AlunoDAO();
-
-        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
-        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
-        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
-
+    private void configuraBotaoSalvar() {
         Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nome = campoNome.getText().toString();
-                String telefone = campoTelefone.getText().toString();
-                String email = campoEmail.getText().toString();
-
-                Aluno alunoCriado = new Aluno(nome, telefone, email);
-                dao.salva(alunoCriado);
-
-                startActivity(new Intent(FormularioAlunoActivity.this,
-                        ListaAlunosActivity.class));
+                Aluno alunoCriado =  criaAluno();
+                salva(alunoCriado);
             }
         });
+    }
+
+    private void inicializacaoDosCampos() {
+        campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+    }
+
+    private void salva(Aluno aluno) {
+        dao.salva(aluno);
+        finish();
+    }
+
+    private Aluno criaAluno() {
+        String nome = campoNome.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        return new Aluno(nome, telefone, email);
     }
 }
 
--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -13,34 +13,49 @@ import com.example.agenda.R;
 import com.example.agenda.dao.AlunoDAO;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class ListaAlunosActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Lista de alunos";
+    private final AlunoDAO dao = new AlunoDAO();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        AlunoDAO dao = new AlunoDAO();
+        setTitle(TITULO_APPBAR);
 
-        setTitle("Lista de alunos");
+        configuraFabNovoAluno();
 
+
+    }
+
+    private void configuraFabNovoAluno() {
         FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
         botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ListaAlunosActivity.this,
-                        FormularioAlunoActivity.class));
+                abreformularioAlunoActivity();
             }
         });
+    }
 
+    private void abreformularioAlunoActivity() {
+        startActivity(new Intent(this,
+                FormularioAlunoActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        configuraLista();
+    }
+
+    private void configuraLista() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 dao.todos()));
-
     }
 }
