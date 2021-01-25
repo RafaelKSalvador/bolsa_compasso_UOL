f54fbdee3411f5ab14cc2609819228fec00e1f40
RafaelKSalvador <rafaelk.salvador@gmail.com>
2021-01-25 12:13:00 -0300

    Recycler View parte 1: Listas flexíveis e performáticas

---
+++ app/src/main/java/com/example/ceep/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,43 @@
+package com.example.ceep.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.ceep.R;
+import com.example.ceep.dao.NotaDAO;
+import com.example.ceep.model.Nota;
+import com.example.ceep.ui.recyclerview.adapter.ListaNotasAdapter;
+
+import java.util.List;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+        List<Nota> todasNotas = notasDeExemplo();
+        configuraRecyclerView(todasNotas);
+    }
+
+    private List<Nota> notasDeExemplo() {
+        NotaDAO dao = new NotaDAO();
+        for (int i = 1; i <= 10000; i++) {
+            dao.insere(new Nota("Título" + i,
+                    "Descrição" + i));
+        }
+        return dao.todos();
+    }
+
+    private void configuraRecyclerView(List<Nota> todasNotas) {
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
+        configuraAdapter(todasNotas, listaNotas);
+    }
+
+    private void configuraAdapter(List<Nota> todasNotas, RecyclerView listaNotas) {
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+    }
+}
